216f4b2781b86f85eb1949b72c791b7b81cb13f2
14165 <rounald.riadandrawes@ontariotechu.net>
2023-10-30 18:38:25 -0400

    settings setup

--- stockpredictor/stockpredictor/settings.py
+++ stockpredictor/stockpredictor/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4q_@_*8u5d9p_s&$-5o=km(*n)2&01j8$fw@f9^yw@ef0d=85f'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
